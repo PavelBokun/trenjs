f8a4c3f59e4c641f85bed4f4df81b321638939e6
PavelB <pavel.snabteh@gmail.com>
2024-04-22 23:08:54 +0300

    second

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlise from "./features/userSlise";
+import todoSlice from "./features/todo2";
 
-import { configureStore } from '@reduxjs/toolkit';
-import userSlise from './features/userSlise';
- const store=configureStore({
-    reducer:{
-        user: userSlise
-    }
-})
+const store = configureStore({
+  reducer: {
+    user: userSlise,
+    todo: todoSlice,
+  },
+});
 
 export default store;
